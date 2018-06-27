31485b36b9703106a54080e72d539fb810f6d0b9
Juno Lee <lejuno80@gmail.com>
2018-06-26 19:05:39 -0500

    buiding added

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 import Listings from '../components/Listings/Listings.js';
 
+import Buildings from '../components/Building/Building.js';
 import './App.css';
 
 class App extends Component {
   render () {
     return (
-      <Listings />
+      <div className="App">
+        <Listings />
+        <Buildings />
+      </div>
     );
   }
 }
---
+++ src/components/Building/Building.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Building.css';
+
+class Buildings extends React.Component {
+  render () {
+    return (
+      <div className="Building">
+        <h2>Building</h2>
+      </div>
+    );
+  }
+}
+
+export default Buildings;
